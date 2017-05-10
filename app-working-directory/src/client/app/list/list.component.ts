6eb5de844857c381f6bae41874d0993c11803a5b
Patrick Charles-Lundaahl <pc.lundaahl@gmail.com>
2017-05-10 11:21:21 -0700

    Initial HammerJS import added

--- app-working-directory/src/client/app/list/list.component.ts
+++ app-working-directory/src/client/app/list/list.component.ts
@@ -19,29 +19,36 @@ const LIST_ITEMS: ItemList[] = [
     new ItemList("Aubergine", 76)
 ];
 
+// Constants for interfacing with HammerJS's gestures
+const SWIPE = {
+    LEFT: 'swipeleft',
+    RIGHT: 'swiperight',
+    UP: 'swipeup',
+    DOWN: 'swipedown'
+}
+
 /**
  * This class represents the lazy loaded HomeComponent.
  */
 @Component({
-  moduleId: module.id,
-  selector: 'gb-list',
-  templateUrl: 'list.component.html',
-  styleUrls: ['list.component.css'],
+    moduleId: module.id,
+    selector: 'gb-list',
+    templateUrl: 'list.component.html',
+    styleUrls: ['list.component.css'],
 })
 
 export class ListComponent {
-
   //Instantiating the array object.
   myList: ItemList[] = LIST_ITEMS;
   nameInput: String = "";
   numberInput: number;
 
-  addItem(): void {
-    if(this.nameInput.length > 0, this.numberInput > 0) {
-      this.myList.push(new ItemList(this.nameInput, this.numberInput));
-      this.nameInput = '';
-      this.numberInput = null;
+    addItem(): void {
+        if(this.nameInput.length > 0, this.numberInput > 0) {
+            this.myList.push(new ItemList(this.nameInput, this.numberInput));
+            this.nameInput = '';
+            this.numberInput = null;
+        }
+        console.log(this.myList[1]);
     }
-    console.log(this.myList[1]);
-  }
 }
