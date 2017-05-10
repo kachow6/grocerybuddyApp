250fa6c6c2c91f6a90d02b90693d6684d2c4b7ad
Raly Tan <tan_raly@hotmail.com>
2017-05-10 14:05:00 -0700

    Added checked attribute

--- app-working-directory/src/client/app/list/list.component.ts
+++ app-working-directory/src/client/app/list/list.component.ts
@@ -5,18 +5,20 @@ import { Component } from '@angular/core';
 export class ItemList {
     name: String;
     quantity: number;
+    checked: boolean;
 
-    constructor(name: String, quantity: number) {
+    constructor(name: String, quantity: number, checked: boolean) {
         this.name = name;
         this.quantity = quantity;
+        this.checked = checked;
     }
 }
 
 //Array that contains the items on List page.
 const LIST_ITEMS: ItemList[] = [
-    new ItemList("Avocados", 5),
-    new ItemList("Banana", 3),
-    new ItemList("Aubergine", 76)
+    new ItemList("Avocados", 5, false),
+    new ItemList("Banana", 3, false),
+    new ItemList("Aubergine", 76, false)
 ];
 
 /**
@@ -34,13 +36,33 @@ export class ListComponent {
   myList: ItemList[] = LIST_ITEMS;
   nameInput: String = "";
   numberInput: number;
+  checkedInput: boolean;
 
     addItem(): void {
         if (this.nameInput.length > 0, this.numberInput > 0) {
-            this.myList.push(new ItemList(this.nameInput, this.numberInput));
+            this.myList.push(new ItemList(this.nameInput, this.numberInput, this.checkedInput));
             this.nameInput = '';
             this.numberInput = null;
         }
         console.log(this.myList[1]);
+    }
+
+    checkItem(): void {
+        if(this.checkedInput == false) {
+            this.checkedInput = true;
+        } else if (this.checkedInput == true){
+            this.checkedInput = false;
+        }
+        console.log("______________");
+        console.log(LIST_ITEMS[0]);
+        console.log(LIST_ITEMS[1]);
+        console.log(LIST_ITEMS[2]);
+    }
+
+    //Method for testing item checked state
+    testStuff(): void {
+        console.log(LIST_ITEMS[0]);
+        console.log(LIST_ITEMS[1]);
+        console.log(LIST_ITEMS[2]);
     }
 }
