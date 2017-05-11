bfe897f858c81a2343dd39050df8fa592cd7aba7
Carlo Mendoza <carlomendoza89@gmail.com>
2017-05-10 17:09:59 -0700

    added functionality to buddy pic and quotes in the menu

--- app-working-directory/src/client/app/shared/header/header.component.ts
+++ app-working-directory/src/client/app/shared/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BUDDY_QUOTES } from  './header.menu.buddyquotes';
+import { BUDDY_PICS } from  './header.menu.buddypics';
 
 /**
  * This class represents the navigation bar component.
  */
+
 @Component({
   moduleId: module.id,
   selector: 'gb-header',
@@ -16,6 +19,12 @@ export class HeaderComponent {
 	bodyBg   : string;
 	extended : boolean;
 
+	buddyQuote: string;
+	quoteIndex: number = 0;
+
+	picIndex: number = Math.floor((Math.random() * BUDDY_PICS.length));
+	buddyPic: string = BUDDY_PICS[this.picIndex];
+
 	openNav(): void {
 		this.push 	= this.extWidth;
 		this.bodyBg  = "rgba(0, 0, 0, 0.8)";
@@ -28,5 +37,24 @@ export class HeaderComponent {
 
 	ngOnInit() {
 		this.closeNav();
+	}
+
+	changeQuote(): void {
+		this.buddyQuote = BUDDY_QUOTES[this.quoteIndex];
+		this.quoteIndex++;
+		if(this.quoteIndex == BUDDY_QUOTES.length){
+			this.quoteIndex = 0;
+		}
+	}
+
+	changeBuddy(): void {
+		let number1 = this.picIndex;
+		this.buddyPic = BUDDY_PICS[number1];
+		this.picIndex = Math.floor((Math.random() * BUDDY_PICS.length));
+		let number2= this.picIndex;
+		while(number1 == number2){
+			number2=Math.floor((Math.random() * BUDDY_PICS.length));
+		}
+		this.picIndex = number2;
 	}
 }
---
+++ app-working-directory/src/client/app/shared/header/header.menu.buddyquotes.ts
@@ -0,0 +1,8 @@
+export const BUDDY_QUOTES: string[] = [
+	"Did you know that 47% of food waste in Metro Vancouver is caused by consumers?", 
+	"Have you been using all of your groceries?", 
+	"Always check your fridge to make sure your food is fresh!",
+	"You can create a new grocery list in your HOME page",
+	"Don't forget to check out your groceries after you buy them!"
+
+]
