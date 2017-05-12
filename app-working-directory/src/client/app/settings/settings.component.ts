06404994a63babaa79174d7eed2f476328ec31b8
RalyT <tan_raly@hotmail.com>
2017-05-11 18:34:59 -0700

    Coded functionaltiy into the account settings

--- app-working-directory/src/client/app/settings/settings.component.ts
+++ app-working-directory/src/client/app/settings/settings.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component }    from '@angular/core';
+import { UserService }  from '../shared/user-service/user.service';
+import { User }         from '../shared/user-service/user';
+import { Router }       from '@angular/router';
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -12,12 +15,19 @@ import { Component } from '@angular/core';
 
 export class SettingsComponent {
     passwordcomplex: boolean;
-    passwordconfirmed: boolean;
+    passwordconfirmed: boolean = false;
     passwordmessage: string;
     confirmpasswordmessage: string ='';
     userpassword: string;
     userconfirmpassword: string ='';
+    nameInput: string = "";
+    emailInput: string = "";
 
+    constructor(private router: Router){
+
+    }
+
+    //Method for checking if the password is complex
     checkComplexity(userpass: string): void {
 
         let hasUpperCase = /[A-Z]/.test(userpass);
@@ -26,17 +36,13 @@ export class SettingsComponent {
         let hasNonalphas = /\W/.test(userpass);
 
         if (hasUpperCase && hasLowerCase && hasNumbers && hasNonalphas) {
-            this.passwordmessage = '';
             this.passwordcomplex = true;
         } else {
-            this.passwordmessage = 'Must contain at least one uppercase and '
-                                 + 'lower case letter as well as a number and '
-                                 + 'a special character (ie. $, #, !, %)';
             this.userpassword = null;
         }
-        console.log('click');
     }
 
+    //Method for making sure both passwords are the same
     confirmPassword(pass1: string, pass2: string, ignoreblank: boolean): void {
 
         if(ignoreblank === true && pass2.length === 0) {
@@ -52,4 +58,29 @@ export class SettingsComponent {
         }
     }
 
+    //Method for clearing the account fields.
+    clearFields(): void {
+        this.nameInput = "";
+        this.emailInput = "";
+        this.userpassword = "";
+        this.userconfirmpassword = "";
+    }
+
+    //A method for submitting the new account information
+    changeInfo(): void {
+        if(this.nameInput.length > 0 && this.emailInput.length > 0
+            && this.passwordcomplex == true && this.passwordconfirmed == true) {
+
+                UserService.user.username = this.nameInput;
+                UserService.user.email = this.emailInput;
+                UserService.user.password = this.userpassword;
+
+                this.clearFields();
+                console.log('ACCOUNT OVERWRITE SUCCESS');
+                this.router.navigateByUrl('/');
+        } else {
+            console.log('ACCOUNT OVERWRITE FAILED');
+        }
+        console.log(UserService.user);
+    }
 }
