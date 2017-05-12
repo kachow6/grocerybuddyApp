869113abed07febeccbf728e9d4e89d24940d882
Carlo Mendoza <carlomendoza89@gmail.com>
2017-05-11 20:19:53 -0700

    added notification modal functions

--- app-working-directory/src/client/app/shared/header/header.component.ts
+++ app-working-directory/src/client/app/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BUDDY_QUOTES } from  './header.menu.buddyquotes';
 import { BUDDY_PICS } from  './header.menu.buddypics';
+import { UserService }      from '../user-service/user.service';
+import { FridgeItem }     from '../user-service/user';
 
 /**
  * This class represents the navigation bar component.
@@ -19,11 +21,35 @@ export class HeaderComponent implements OnInit {
     bodyBg   : string;
     extended : boolean;
 
-    buddyQuote: string;
+    expiringItems: FridgeItem[];
+
+    //States for the progress bar
+    stateDanger: string = 'progress-bar-danger';
+    stateWarning: string = 'progress-bar-warning';
+    stateSuccess: string = 'progress-bar-success';
+    itemState: string = '';
+
+    buddyQuote: string = BUDDY_QUOTES[0];
     quoteIndex: number = 0;
 
     picIndex: number = Math.floor((Math.random() * BUDDY_PICS.length));
     buddyPic: string = BUDDY_PICS[this.picIndex];
+
+    constructor(private userService: UserService) {
+        this.expiringItems = userService.getFridge();
+    }
+
+    pullExpiring(list: FridgeItem[]): FridgeItem[]{
+        let templist: FridgeItem[] = [];
+        for(let fridgeItem of list){
+            let exp = fridgeItem.expiration/fridgeItem.maxAge;
+
+            if(exp < 0.33) {
+                templist.push(fridgeItem);
+            }
+        }
+        return templist;
+    }
 
     openNav(): void {
         this.push     = this.extWidth;
@@ -57,4 +83,18 @@ export class HeaderComponent implements OnInit {
         }
         this.picIndex = number2;
     }
+
+    // Method for calculating the expiration bar colour
+    calculateExp(max: number, expiration: number): string {
+        if (expiration / max < 0.33){
+            return this.stateDanger;
+        } else if (expiration / max >= 0.66){
+            return this.stateSuccess;
+        } else {
+            return this.stateWarning;
+        }
+    }
+
 }
+
+
