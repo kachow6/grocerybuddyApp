d0d66014e88ea813219cf01a4ec76fef49450478
pclundaahl <pc.lundaahl@gmail.com>
2017-05-12 18:25:04 -0700

    Quick fix to make sure default route redirects to something that loads

--- app-working-directory/src/client/app/app-routing.module.ts
+++ app-working-directory/src/client/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forRoot([
+        {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: '/main'
+        }
       /* define app module routes here, e.g., to lazily load a module
          (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
        */
