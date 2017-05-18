f81185db4783edc4be544d133d545203380ddc78
pclundaahl <pc.lundaahl@gmail.com>
2017-05-17 17:50:45 -0700

    Half way there! (Living on a prayer)

--- current-app-wd/src/app/app-routing.module.ts
+++ current-app-wd/src/app/app-routing.module.ts
@@ -3,23 +3,32 @@ import { RouterModule } from '@angular/router';
 import { ConnectComponent } from './auth/connect.component';
 import { RegisterComponent } from './auth/register.component';
 
+import { LoggedInGuard } from './shared/user-service/logged-in-guard.service';
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot([
-        {
-            path: '',
-            pathMatch: 'full',
-            redirectTo: '/main'
-        },
-        { path: 'login', component: ConnectComponent },
-        { path: 'register', component: RegisterComponent }
-      /* define app module routes here, e.g., to lazily load a module
-         (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
-       */
-    ])
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: '/main',
+            },
+            {
+                path: 'login',
+                component: ConnectComponent,
+                // canActivate: [ LoggedInGuard ]
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+                // canActivate: [ LoggedInGuard ]
+            }
+            /* define app module routes here, e.g., to lazily load a module
+                 (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
+             */
+        ])
+    ],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
 
---
+++ current-app-wd/src/app/shared/user-service/logged-in-guard.service.ts
@@ -0,0 +1,45 @@
+import { Injectable }          from '@angular/core';
+import { CanActivate,
+         Router,
+         ActivatedRouteSnapshot,
+         RouterStateSnapshot
+                             } from '@angular/router';
+
+import { Observable }          from 'rxjs/Observable';
+import                              'rxjs/add/observable/of';
+import                              'rxjs/add/operator/do';
+import                              'rxjs/add/operator/delay';
+
+import { AngularFireAuth }     from 'angularfire2/auth';
+import * as firebase           from 'firebase/app';
+
+@Injectable()
+export class LoggedInGuard implements CanActivate {
+    private static _loggedIn: boolean = false;
+
+    // Constructor
+    constructor(public router: Router,
+                public afAuth: AngularFireAuth) {
+        let testVar = this.afAuth.authState
+                          .map(userSnap => { return Boolean(userSnap) })
+                          .do(userSnap => console.log(userSnap))
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        let nextUrl: string = route.url.toString();
+        let curUrl: string = state.url.toString();
+
+        console.log("next:    " + nextUrl);
+        console.log("current: " + curUrl);
+
+        return this.afAuth.authState                                // Observable. Emits an event every time it receives a snapshot of data from the auth server.
+                   .do(userSnap => {console.log(userSnap)})         // Print out the snapshot of the user's authentication
+                   .map(userSnap => {return Boolean(userSnap) })    // Map all possible data snapshots to boolean.
+                   .do(userSnap => {console.log(userSnap)})         // Print the result of that mapping.
+                   .do(userSnap => {
+                       if (!userSnap) {this.router.navigateByUrl('login')};
+                   });
+
+    }
+}
