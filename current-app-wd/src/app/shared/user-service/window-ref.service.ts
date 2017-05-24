648767248c2328fdf75529394f7622c1d185a3a6
pclundaahl <pc.lundaahl@gmail.com>
2017-05-23 22:40:34 -0700

    Scroll errors should be fixed now

---
+++ current-app-wd/src/app/shared/user-service/window-ref.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+function _window() : any {
+   // return the global native browser window object
+   return window;
+}
+
+@Injectable()
+export class WindowRefService {
+
+  constructor() { }
+
+  get nativeWindow() : any {
+    return _window();
+  }
+}
